f675b92830df88912dba8ae694c23f64a40e17c3
bogota1305 <jd.bogota@uniandes.edu.co>
2023-09-28 14:11:28 -0500

    editar alarma

--- modal.js
+++ modal.js
@@ -1,83 +1,27 @@
-const btnAgregarAlarma = document.getElementById("btn-agregar-alarma");
 const btnEditarAlarma = document.getElementById("btn-editar-alarma");
-const btnEliminarAlarma = document.getElementById("btn-eliminar-alarma");
-
-const btnCerrarModalAgregar = document.getElementById("btn-cerrar-modal-agregar");
 const btnCerrarModal = document.getElementById("btn-cerrar-modal");
-const btnCerrarModalEliminar = document.getElementById("btn-cerrar-modal-eliminar");
-
-
-const btnCancelarModalAgregar = document.getElementById("btn-cancelar-modal-agregar");
 const btnCancelarModal = document.getElementById("btn-cancelar-modal");
-const btnCancelarModalEliminar = document.getElementById("btn-cancelar-modal-eliminar");
-
-const btnAceptarModalAgregar = document.getElementById("btn-aceptar-modal-agregar");
 const btnAceptarModal = document.getElementById("btn-aceptar-modal");
-const btnAceptarModalEliminar = document.getElementById("btn-aceptar-modal-eliminar");
-
-const modal_agregar = document.getElementById("modal-agregar")
 const modal_editar = document.getElementById("modal-editar");
-const modal_eliminar = document.getElementById("modal-eliminar");
-
-btnAgregarAlarma.addEventListener("click", () => {
-  modal_agregar.showModal();
-});
+// const alarma_1 = document.getElementById("alarma-1");
 
 btnEditarAlarma.addEventListener("click", () => {
     modal_editar.showModal();
-});
-
-btnEliminarAlarma.addEventListener("click", () => {
-  modal_eliminar.showModal();
-});
-
-btnCerrarModalAgregar.addEventListener("click", () => {
-  modal_agregar.close();
-})
+  });
 
 btnCerrarModal.addEventListener("click", () => {
   modal_editar.close();
-});
-
-btnCancelarModalAgregar.addEventListener("click", () => {
-  modal_agregar.close();
 });
 
 btnCancelarModal.addEventListener("click", () => {
   modal_editar.close();
 });
 
-btnAceptarModalAgregar.addEventListener("click", () => {
-  modal_agregar.close();
-});
-
 btnAceptarModal.addEventListener("click", () => {
   modal_editar.close();
+  // ocultar();
 });
 
-
-
-
-
-
-
-btnEliminarAlarma.addEventListener("click", () => {
-  modal_eliminar.showModal();
-});
-
-btnCerrarModalEliminar.addEventListener("click", () => {
-  modal_eliminar.close();
-});
-
-btnCancelarModalEliminar.addEventListener("click", () => {
-  modal_eliminar.close();
-});
-
-btnAceptarModalEliminar.addEventListener("click", () => {
-  modal_eliminar.close();
-  ocultar();
-});
-
-function ocultar() {
-  document.getElementById("alarma-3").style.display = "none";
-}
+// function ocultar(){
+//   document.getElementById('alarma-3').style.display = 'none';
+// }
