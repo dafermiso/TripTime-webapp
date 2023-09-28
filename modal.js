93b8dc1567d9997e2d0643f13d513aa1626388cf
bogota1305 <jd.bogota@uniandes.edu.co>
2023-09-28 14:16:36 -0500

    eliminar alarma

--- modal.js
+++ modal.js
@@ -1,27 +1,27 @@
-const btnEditarAlarma = document.getElementById("btn-editar-alarma");
+const btnEliminarAlarma = document.getElementById("btn-eliminar-alarma");
 const btnCerrarModal = document.getElementById("btn-cerrar-modal");
 const btnCancelarModal = document.getElementById("btn-cancelar-modal");
 const btnAceptarModal = document.getElementById("btn-aceptar-modal");
-const modal_editar = document.getElementById("modal-editar");
+const modal = document.getElementById("modal");
 // const alarma_1 = document.getElementById("alarma-1");
 
-btnEditarAlarma.addEventListener("click", () => {
-    modal_editar.showModal();
+btnEliminarAlarma.addEventListener("click", () => {
+    modal.showModal();
   });
 
 btnCerrarModal.addEventListener("click", () => {
-  modal_editar.close();
+  modal.close();
 });
 
 btnCancelarModal.addEventListener("click", () => {
-  modal_editar.close();
+  modal.close();
 });
 
 btnAceptarModal.addEventListener("click", () => {
-  modal_editar.close();
-  // ocultar();
+  modal.close();
+  ocultar();
 });
 
-// function ocultar(){
-//   document.getElementById('alarma-3').style.display = 'none';
-// }
+function ocultar(){
+  document.getElementById('alarma-3').style.display = 'none';
+}
